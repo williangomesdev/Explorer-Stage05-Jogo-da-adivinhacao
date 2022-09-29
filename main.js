b1a3eff3dadab9214d9d992e6731cad4fc65a288
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-09-29 17:08:51 -0300

    Aplicando a lógica

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
+const randomNumber = Math.round(Math.random() * 10);
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const screen2title = document.querySelector(".screen2 h2");
+let xAttempts = 1;
+
+console.log(randomNumber);
+
 function handleOnClick(event) {
   event.preventDefault();
 
   const inputNumber = document.querySelector("#inputNumber");
-  console.log(inputNumber.value);
+
+  if (Number(inputNumber.value) == randomNumber) {
+    screen1.classList.add("hide");
+    screen2.classList.remove("hide");
+
+    if (xAttempts == 1) {
+      screen2title.innerText = `Parabéns,acertou na ${xAttempts}º tentativa!`;
+    } else {
+      screen2title.innerText = `Parabéns, acertou em ${xAttempts} tentativas!`;
+    }
+  } else {
+    window.alert("Que pena, infelizmente você errou! tente novamente!");
+  }
+
+  xAttempts++;
+}
+
+function handleBackOnClick(event) {
+  event.preventDefault();
+  screen2.classList.add("hide");
+  screen1.classList.remove("hide");
 }
