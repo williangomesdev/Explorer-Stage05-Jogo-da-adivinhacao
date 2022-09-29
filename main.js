a4396bfaad2d7309fb8bf8a8d58c48577cb411a4
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-09-29 17:27:58 -0300

    Refatorando

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const inputNumber = document.querySelector("#inputNumber");
 const randomNumber = Math.round(Math.random() * 10);
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
@@ -6,17 +7,17 @@ const btnTry = document.querySelector("#btnTry");
 const btnReset = document.querySelector("#btnReset");
 let xAttempts = 1;
 
-console.log(randomNumber);
+//Eventos
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
 
 //callback function
 function handleTryClick(event) {
-  event.preventDefault();
+  event.preventDefault(); //não faça o padrão
 
-  const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide");
-    screen2.classList.remove("hide");
+    handleToggleScreen();
 
     if (xAttempts == 1) {
       screen2title.innerText = `Parabéns,acertou na ${xAttempts}º tentativa!`;
@@ -31,13 +32,12 @@ function handleTryClick(event) {
   xAttempts++;
 }
 
-function handleResetClick(event) {
-  event.preventDefault();
-  screen2.classList.add("hide");
-  screen1.classList.remove("hide");
-  xAttempts = 1;
+function handleToggleScreen() {
+  screen2.classList.toggle("hide");
+  screen1.classList.toggle("hide");
 }
 
-//Eventos
-btnTry.addEventListener("click", handleTryClick);
-btnReset.addEventListener("click", handleResetClick);
+function handleResetClick() {
+  handleToggleScreen();
+  xAttempts = 1;
+}
